2dd29885f229bf28e577035809c9f4083e8a47ca
tathyadoot-png <tathyadoot@gmail.com>
2026-03-17 15:51:28 +0530

    aa

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import LoginPage from "./login/page";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     dfgdsvgrdegerg
-    </div>
+    <>
+      <LoginPage />
+    </>
   );
 }
